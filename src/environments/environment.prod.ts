b0abc17a4bc69974901a145ea9fdd87a551a7e12
Eric Hamacher <erichamacher@protonmail.com>
2022-06-11 12:37:48 -0500

    added prod Cognito config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ import { LogLevel } from "angular-auth-oidc-client";
 export const environment = {
   production: true,
   baseurl: 'https://api.hvgn.net/prod',
-  oidc_authority: 'https://offeringsolutions-sts.azurewebsites.net',
-  oidc_clientId: '',
-  oidc_postLogoutRedirectUri: '',
-  oidc_redirectUrl: '',
-  oidc_userPoolId: 'us-east-1_7R0XneWgA',
-  oidc_authWellknownEndpointUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_7R0XneWgA/.well-known/openid-configuration',
+  oidc_authority: 'https://hvgn-prod-auth.auth.us-east-1.amazoncognito.com/',
+  oidc_clientId: '<clientId>',
+  oidc_postLogoutRedirectUri: 'https://hvgn.net',
+  oidc_redirectUrl: 'https://hvgn.net/login/callback',
+  oidc_userPoolId: '<userPoolId>',
+  oidc_authWellknownEndpointUrl: `https://cognito-idp.us-east-1.amazonaws.com/<userPoolId>/.well-known/openid-configuration`,
   oidc_revoke_endpoint_path: '/oauth2/revoke',
   oidc_logoff_endpoint_path: '/logout',
   oidc_logLevel: LogLevel.Error
